9749618beafa55ae9a37e877894190216069a8b2
phoebe0412 <phoebe19810412@gmail.com>
2021-07-05 16:07:29 +0800

    update

--- practice-vue/src/views/About.vue
+++ practice-vue/src/views/About.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>姓：<span><input type="text" v-model="firstName"></span></p>    
-    <p>名：<span><input type="text" v-model="lastName"></span></p>   
+    <p>
+      姓：<span><input type="text" v-model="firstName" /></span>
+    </p>
+    <p>
+      名：<span><input type="text" v-model="lastName" /></span>
+    </p>
     <p>{{ fullName }}</p>
-    <hr>
-    
+    <hr />
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue, Watch } from "vue-property-decorator";
 
-
 @Component({
   components: {},
 })
 export default class About extends Vue {
-
-  @Watch ('firstName')
-  private watchFirstName(val:any){
-    this.fullName = val + ' ' + this.lastName
+  @Watch("firstName")
+  private watchFirstName(val: any) {
+    this.fullName = val + " " + this.lastName;
   }
 
-  @Watch ('lastName')
-  private watchLastName(val:any){
-    this.fullName = this.firstName + ' ' + val
+  @Watch("lastName")
+  private watchLastName(val: any) {
+    this.fullName = this.firstName + " " + val;
   }
 
-  private firstName = ""
-  private lastName = ""
-  private fullName = ""
-
+  private firstName = "";
+  private lastName = "";
+  private fullName = "";
 }
 </script>
