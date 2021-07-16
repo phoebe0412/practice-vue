1ec49d6ec976b5e05610e76ebdbce85be0100078
phoebe0412 <phoebe19810412@gmail.com>
2021-07-16 16:35:10 +0800

    dialog

---
+++ practice-vue/src/components/Dialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-dialog :visible.sync="isShowDialog" :width="`${width}%`" center>
+      <div slot="title"><slot name="title"></slot></div>
+      <main>
+        <slot name="dialogContent"></slot>
+      </main>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="handlerSubmit">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue, Watch } from "vue-property-decorator";
+@Component
+export default class Dialog extends Vue {
+  @Prop() private width!: string;
+  @Prop({ default: false }) private dialogVisible!: boolean;
+  @Emit("submit")
+  private handlerSubmit() {
+    this.isShowDialog = false;
+  }
+  @Watch("dialogVisible")
+  private watchDialog(newVal: any) {
+    if (newVal) {
+      this.isShowDialog = newVal;
+    }
+  }
+
+  private isShowDialog = false;
+}
+</script>
